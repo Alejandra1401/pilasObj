d66f8b48c18954bacf22aededad3ed0a8f33dcd2
Sala K 302 <salak302@salasfr.itm.edu.co>
2025-03-17 06:19:38 -0500

    listo

--- metodos.java
+++ metodos.java
@@ -1,77 +1,68 @@
 import java.util.Stack;
-
 import javax.swing.JOptionPane;
 
 public class metodos 
 {
-    public Stack<Vehiculo> llenarpila()
+    public void LlenarPila() 
     {
-        Stack<Vehiculo> pila = new Stack<>();//declara variable pila de tipo obj, la pila es una coleccion de objeto
+        Stack<Vehiculo> pila = new Stack<>();//Declara variable pila de tipo obj, la pila es una coleccion de objeto
         boolean continuar = true;
         String agregar = "";
         while (continuar) 
         {
             Vehiculo o = new Vehiculo();
-            o.setMarca(JOptionPane.showInputDialog("Ingrese la marca "));
-            o.setColor(JOptionPane.showInputDialog("Ingrese el color "));
-            o.setPrecio(Double.parseDouble(JOptionPane.showInputDialog("Ingrese el precio")));//esto con dooble, int, float. debo convertirlo al tipo de dato
+            o.setMarca(JOptionPane.showInputDialog("Ingrese La marca"));
+            o.setColor(JOptionPane.showInputDialog("Ingrese La Color"));
+            o.setPrecio(Double.parseDouble(JOptionPane.showInputDialog("Ingrese el precio")));
             pila.push(o);//Aqui llevo los datos a la pila 
-            agregar = JOptionPane.showInputDialog(null, "Desea ingresar mas registros: S/N");
-            if ( agregar.equalsIgnoreCase("N"))
+            agregar = JOptionPane.showInputDialog("Desea agregar mas Registros S/N");
+            if (agregar.equalsIgnoreCase("N"))
             {
                 continuar = false;
             }
         }
-        return pila;
+        MostrarPila(pila);
+        int opt = Integer.parseInt(JOptionPane.showInputDialog("ingrese 1: consultar, 2: eliminar ,3 modificar "));
+        pila = AccionesRegistro(pila, opt);
+        MostrarPila(pila);
     }
-    public void MostrarPila(Stack<Vehiculo> pila)
+
+    public void MostrarPila(Stack<Vehiculo> pila) 
     {
-        for (Vehiculo o : pila)//Deshapilando para mostrar(es lo mismo que un for normal)
+        for (Vehiculo o : pila) //Deshapilando para mostrar(es lo mismo que un for normal)
         {
-            System.out.println( "Marca:" + o.getMarca());
+            System.out.println("Marca: " + o.getMarca());
             System.out.println("Color: " + o.getColor());
-            System.out.println("Precio:" + o.getPrecio());
+            System.out.println("Precio " + o.getPrecio());
             System.out.println();
         }
-        int opt = Integer.parseInt(JOptionPane.showInputDialog("Ingrese 1 encontrar, 2 eliminar, 3 modificar"));
-        accionesRegistro(pila, opt);
-
     }
-    public Stack<Vehiculo> accionesRegistro (Stack<Vehiculo> pila, int opt)
+
+    public Stack<Vehiculo> AccionesRegistro(Stack<Vehiculo> pila, int opt) 
     {
-        String dato;
-        if (opt ==1)
+        String dato = "";
+        if (opt == 1) 
         {
-            dato = JOptionPane.showInputDialog("Ingrese el registro que desea consultar");
+            dato = JOptionPane.showInputDialog("Ingrese el registro a consultar");
+        } else if (opt == 2) {
+            dato = JOptionPane.showInputDialog("Ingrese el registro al que desea eliminar");
+        } else {
+            dato = JOptionPane.showInputDialog("Ingrese el registro al que desea Modificar");
         }
-        else if (opt ==2)
-        {
-            dato = JOptionPane.showInputDialog("Ingrese el registro que desea eliminar");
-        }
-        else
-        {
-            dato = JOptionPane.showInputDialog("Ingrese el registro que desea modificar");
-        }
-       
-        for (Vehiculo carrito : pila)//Deshapilando 
-        {
-            if(carrito.getMarca().equalsIgnoreCase(dato))
-            {
-              if(opt == 1)
-              {
-                System.out.println(
-                    "El registro se encuentra y es: " + carrito.getMarca() + " " + carrito.getPrecio());
-              }
-              else if (opt == 2)
-              {
-                pila.remove(carrito);
-              } 
-              else
-              {
-                carrito.setMarca(JOptionPane.showInputDialog("Ingrese la marca"));
-              }
+        for (Vehiculo vehiculo : pila) {
+            if (vehiculo.getMarca().equalsIgnoreCase(dato)) {
+                if (opt == 1) {
+                    System.out.println(
+                            "el registro se encuentras y es: " + vehiculo.getMarca() + " " + vehiculo.getPrecio());
+                } else if (opt == 2) {
+                    pila.remove(vehiculo);
+                } else {
+                    vehiculo.setMarca(JOptionPane.showInputDialog("Ingrese La marca"));
+
+                }
             }
         }
+
         return pila;
     }
     
--- principal.java
+++ principal.java
@@ -1,13 +1,10 @@
-import java.util.Stack;
 
 public class principal 
 {
     public static void main(String[] args) 
     {
         metodos m = new metodos();
-        Stack<Vehiculo> pila = m.llenarpila();
-        m.MostrarPila(pila);
-        
+        m.LlenarPila();
     }
     
 }
